e403fa3bbd694ec3f761043d5e0b182d1a127639
OHASHI Hideya <ohachige@gmail.com>
2010-03-28 14:40:55 +0900

    scrape

--- lib/torrentcache.rb
+++ lib/torrentcache.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'fileutils'
 require 'bencode'
 require 'digest/sha1'
+require 'net/http'
+require 'open-uri'
 
 module TorrentCache
 
@@ -17,12 +19,16 @@ module TorrentCache
         # TODO
         torrentf = File.join(SETTING_DIR, 'test.torrent')
         torrent = BEncode.load(File.read(torrentf))
-        announce = torrent["announce"]
-        info = torrent["info"]
-p info["pieces"].size
-        piece_length = info["piece length"]
-        info_hash = Digest::SHA1.hexdigest(BEncode.dump(info)) # TODO
-p info_hash
+        announce = torrent['announce']
+        info = torrent['info']
+        pieces = info['pieces'].each_byte.each_slice(20).map{|cs|cs.pack('c*')}
+        piece_length = info['piece length']
+        info_hash = Digest::SHA1.digest(BEncode.dump(info))
+        scrape = announce.gsub(/\/announce\z/, '/scrape')
+        res = open("#{scrape}?info_hash=#{URI.encode(info_hash)}") do |fd|
+          BEncode.load(fd.read)
+        end
+p res
       end
     end
   end

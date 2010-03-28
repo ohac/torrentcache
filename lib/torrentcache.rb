3c2ea9b0788b60eb871af46de89554e6e5326ef8
OHASHI Hideya <ohachige@gmail.com>
2010-03-28 14:19:20 +0900

    calculate info_hash

--- lib/torrentcache.rb
+++ lib/torrentcache.rb
@@ -1,11 +1,28 @@
 require 'rubygems'
+require 'fileutils'
 require 'bencode'
+require 'digest/sha1'
 
 module TorrentCache
+
+  HOME_DIR = ENV['HOME']
+  SETTING_DIR = "#{HOME_DIR}/.torrentcache"
+  unless File.exist? SETTING_DIR
+    FileUtils.mkdir SETTING_DIR
+  end
+
   module Client
     class << self
       def run
-        p :run # TODO
+        # TODO
+        torrentf = File.join(SETTING_DIR, 'test.torrent')
+        torrent = BEncode.load(File.read(torrentf))
+        announce = torrent["announce"]
+        info = torrent["info"]
+p info["pieces"].size
+        piece_length = info["piece length"]
+        info_hash = Digest::SHA1.hexdigest(BEncode.dump(info)) # TODO
+p info_hash
       end
     end
   end

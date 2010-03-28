41e749f9c8265e92f2ae038e9fef286cd5c35f87
OHASHI Hideya <ohachige@gmail.com>
2010-03-28 16:25:22 +0900

    save

--- lib/torrentcache.rb
+++ lib/torrentcache.rb
@@ -15,34 +15,37 @@ module TorrentCache
 
   module Client
     class << self
+      def scrape(announce, info_hash)
+        params = "info_hash=#{URI.encode(info_hash)}"
+        scrape = announce.gsub(/\/announce\z/, '/scrape')
+        res = open("#{scrape}?#{params}"){|fd| BEncode.load(fd.read)}
+        status = res['files'][info_hash]
+        [status['complete'], status['downloaded'], status['incomplete']]
+      end
       def run
         # TODO
         torrentf = File.join(SETTING_DIR, 'test.torrent')
         torrent = BEncode.load(File.read(torrentf))
         announce = torrent['announce']
         info = torrent['info']
+        total_size = info['files'].inject(0){|t,i| t + i['length']}
         pieces = info['pieces'].each_byte.each_slice(20).map{|cs|cs.pack('c*')}
         piece_length = info['piece length']
         info_hash = Digest::SHA1.digest(BEncode.dump(info))
+p scrape(announce, info_hash)
         info_hash_u = URI.encode(info_hash)
-        scrape = announce.gsub(/\/announce\z/, '/scrape')
-=begin
-        res = open("#{scrape}?info_hash=#{info_hash_u}") do |fd|
-          BEncode.load(fd.read)
-        end
-p res
-=end
         peer_id = '-TC0000-abcdef12341j'
         params = { :info_hash => info_hash_u, :peer_id => peer_id,
           :port => 34217, :uploaded => 0, :downloaded => 0, :corrupt => 0,
-          :left => 0, :compact => 1, :supportcrypto => 0, :numwant => 200,
-          :key => '1234567890'
+          :left => total_size, :compact => 1, :supportcrypto => 0,
+# :event => 'stopped' # or started, completed
         }.map{|k,v|"#{k}=#{v}"}.join('&')
 
         res = open("#{announce}?#{params}"){|fd| BEncode.load(fd.read)}
         peers = res['peers'].each_byte.each_slice(6).map{|i|i.pack('c*')}
-p res['interval']
-p peers
+        interval = res['interval']
+p peers.map{|i|i.unpack('C4n').join('.')}
+
       end
     end
   end
